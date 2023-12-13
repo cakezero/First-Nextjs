c68065588b25b9648a866198e1c0b7ac6544c9a6
Bright Elom <sareahj.com@gmail.com>
2023-12-13 14:24:47 +0100

    Delete .next directory

--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"dzQAb/HpX5IsoRqWKeiOKdKvNW6cprP+QYqrJFi99mk=\"\n}"
--- .next/transform.js
+++
@@ -1,6 +0,0 @@
-Error.stackTraceLimit = 100;
-global.self = global;
-require("./chunks/node_modules_0f997e._.js");
-require("./chunks/[root of the server]__ee00d3._.js");
-require("./chunks/[node]__next_transform_f200e2.js");
-require("./chunks/[node]__next_transform_289c09.js");
